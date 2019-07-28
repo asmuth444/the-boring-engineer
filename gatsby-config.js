b5dc87a659de2255a8edbb234d7fa472725d4861
Abhishek Roul <asmuth444@gmail.com>
2019-07-28 19:28:47 +0530

    Added some GA setup

--- gatsby-config.js
+++ gatsby-config.js
@@ -70,7 +70,7 @@ module.exports = {
     {
       resolve: `gatsby-plugin-google-analytics`,
       options: {
-        //trackingId: `ADD YOUR TRACKING ID HERE`,
+        trackingId: `UA-81478353-1`,
       },
     },
     `gatsby-plugin-feed`,
